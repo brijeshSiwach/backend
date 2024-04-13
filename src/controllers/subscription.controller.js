804da52b5a635d5858993553eb1bdb17f9cff5be
Brijesh Siwach <brijeshsiwach93@gmail.com>
2024-04-14 00:27:55 +0530

    Added Subscription and Tweet controller

---
+++ src/controllers/subscription.controller.js
@@ -0,0 +1,104 @@
+import { Mongoose, isValidObjectId } from "mongoose";
+import { asyncHandler } from "../utils/asyncHandler";
+import { ApiError } from "../utils/ApiError";
+import { User } from "../models/user.model";
+import { Subscription } from "../models/subscriptions.model";
+import { ApiResponse } from "../utils/ApiResponse";
+
+const toggleSubscription = asyncHandler( async(req, res) => {
+    const channelId = req.params
+    const userId = req.user._id
+
+    if(!isValidObjectId(channelId) || !isValidObjectId(userId)) {
+        throw new ApiError(400, "Invaid Mongoose Object Id")
+    }
+
+    const userChannel = await User.findById(channelId)
+    if(!userChannel) {
+        throw new ApiError(404, "Channle does Not Exist")
+    }
+    const isSubscibed = await Subscription.findOne({
+        channel: channelId,
+        subsciber: userId
+    })
+
+    if(isSubscibed) {
+        await Subscription.findByIdAndDelete(isSubscibed._id)
+
+        const subscribed = await Subscription.findById(isSubscibed._id)
+        if(subscribed) {
+            throw new ApiError(500, "Error While Toggling the channel")
+        }
+
+        return res
+        .status(200)
+        .json(
+            new ApiResponse(200, "", "Channel Unsubscribed Successfully")
+        )
+    }
+    else {
+        const updateSubscription = await Subscription.create({
+            channel: channelId,
+            subsciber: userId
+        })
+
+        if(!updateSubscription) {
+            throw new ApiError(500, "Error while subscribing the Channel")
+        }
+
+        return res
+        .status(200)
+        .json(
+            new ApiResponse(200, updateSubscription, "Channel Subscribed Successfully")
+        )
+    }
+})
+
+const getUserChannelSubscribers = asyncHandler( async(req, res) => {
+    const channelId = req.params
+    const userId = req.user._id
+
+    if(!isValidObjectId(channelId)) {
+        throw new ApiError(400, "Invaid Mongoose Object Id")
+    }
+
+    if(channelId.toString() !== userId.toString()) {
+        throw new ApiError(405, "You are not authorized to access the subscibers list")
+    }
+
+    const subscribers = await Subscription.find({channel: channelId}).select("-createdAt -updatedAt -channel")
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, subscribers, "Subscribers Fetched Successfullly")
+    )
+})
+
+const getSubscibedChannels = asyncHandler( async(req, res) => {
+    const subsciberId = req.params
+    const userId = req.user._id
+
+    if(!isValidObjectId(subsciberId)) {
+        throw new ApiError(400, "Invaid Mongoose Object Id")
+    }
+
+    if(subsciberId.toString() !== userId.toString()) {
+        throw new ApiError(405, "You are not authorized to access the subscibered channel list")
+    }
+
+    const channels = await Subscription.find({subsciber: subsciberId}).select("-createdAt -updatedAt -subscriber")
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, channels, "Channels Fetched Successfullly")
+    )
+
+})
+
+export {
+    toggleSubscription,
+    getUserChannelSubscribers,
+    getSubscibedChannels
+}
---
+++ src/controllers/tweet.controller.js
@@ -0,0 +1,135 @@
+import { isValidObjectId, mongoose } from "mongoose";
+import { asyncHandler } from "../utils/asyncHandler";
+import { ApiError } from "../utils/ApiError";
+import { Tweet } from "../models/tweet.model";
+import { ApiResponse } from "../utils/ApiResponse";
+
+const createTweet = asyncHandler( async(req, res) => {
+    const userId = req.user._id
+    const content = req.body
+
+    if(!isValidObjectId(userId)) {
+        throw new ApiError(400, "Invalid Mongoose ObjectId")
+    }
+
+    if(!content) {
+        throw new ApiError(404, "Content is Required")
+    }
+
+    const tweet = await Tweet.create({
+        content: content,
+        owner: userId
+    })
+
+    if(!tweet) {
+        throw new ApiError(500, "Error while creating tweet")
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, tweet, "Tweet is Created Successfully")
+    )
+})
+
+const getUserTweets = asyncHandler( async(req, res) => {
+    const userId = req.params
+
+    if(!isValidObjectId(userId)) {
+        throw new ApiError(400, "Invalid Mongoose ObjectId")
+    }
+
+    const userTweets = await Tweet.find({ owner: userId })
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, userTweets, "User Tweets is Fetched Successfully")
+    )
+})
+
+const updateTweet = asyncHandler( async(req, res) => {
+    const userId = req.user._id
+    const content = req.body
+    const tweetId = req.params
+
+    if(!isValidObjectId(userId) || !isValidObjectId(tweetId)) {
+        throw new ApiError(400, "Invalid Mongoose ObjectId")
+    }
+
+    if(!content) {
+        throw new ApiError(404, "Content is Required")
+    }
+
+    const isTweetExist = await Tweet.findById(tweetId)
+    
+    if(!isTweetExist) {
+        throw new ApiError(404, "Tweet Does Not Exist")
+    }
+
+    if(isTweetExist.owner.toString() !== userId.toString()) {
+        throw new ApiError(405, "You are Not Authorized to update this Tweet")
+    }
+
+    const tweet = await Tweet.findByIdAndUpdate(
+        tweetId,
+        {
+            $set: {
+                content: content
+            }
+        },
+        {
+            new: true
+        }
+    )
+
+    if(!tweet) {
+        throw new ApiError(500, "Error while Updating tweet")
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, tweet, "Tweet is Updated Successfully")
+    )
+})
+
+const deleteTweet = asyncHandler( async(req, res) => {
+    const userId = req.user._id
+    const tweetId = req.params
+
+    if(!isValidObjectId(userId) || !isValidObjectId(tweetId)) {
+        throw new ApiError(400, "Invalid Mongoose ObjectId")
+    }
+
+    const isTweetExist = await Tweet.findById(tweetId)
+    
+    if(!isTweetExist) {
+        throw new ApiError(404, "Tweet Does Not Exist")
+    }
+
+    if(isTweetExist.owner.toString() !== userId.toString()) {
+        throw new ApiError(405, "You are Not Authorized to update this Tweet")
+    }
+
+    await Tweet.findByIdAndDelete(tweetId)
+    const tweet = await Tweet.findById(tweetId)
+    
+    if(tweet) {
+        throw new ApiError(500, "Error while Deleting tweet")
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, tweet, "Tweet is Deleted Successfully")
+    )
+})
+
+
+export {
+    createTweet,
+    getUserTweets,
+    updateTweet,
+    deleteTweet,
+}
